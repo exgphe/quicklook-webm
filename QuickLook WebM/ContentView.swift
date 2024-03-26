d34fc967df842fbd19197c8ca07b926940e46d14
Xiaolin Wang <exgphe@gmail.com>
2024-03-26 23:27:22 +0100

    Initial Commit

---
+++ QuickLook WebM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickLook WebM
+//
+//  Created by Xiaolin Wang on 26/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickLook WebM/QuickLook_WebMApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLook_WebMApp.swift
+//  QuickLook WebM
+//
+//  Created by Xiaolin Wang on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickLook_WebMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
